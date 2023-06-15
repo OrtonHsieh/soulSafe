3823887f9a53b2d51b9258d91d540c830dca79a7
Orton Hsieh <hugoorton2571dt@gmail.com>
2023-06-15 11:22:36 +0800

    [feat] installed swiftlint via homebrew.

--- SoulSafe/SoulSafe/AppDelegate.swift
+++ SoulSafe/SoulSafe/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by 謝承翰 on 2023/6/14.
 //
 
+//// swiftlint:disable all
 import UIKit
 
 @main
@@ -30,7 +31,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
+//// swiftlint:enable all
