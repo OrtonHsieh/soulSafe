f895672ac6bd14492bccf706c47de1240288b828
Orton Hsieh <hugoorton2571dt@gmail.com>
2023-06-17 17:39:48 +0800

    [feat] Imported GrowingTextView.

--- SoulSafe/SoulSafe/MemoriesViewController/PostViewController/TextAreaView.swift
+++ SoulSafe/SoulSafe/MemoriesViewController/PostViewController/TextAreaView.swift
@@ -6,23 +6,21 @@
 //
 
 import UIKit
+import GrowingTextView
 
 class TextAreaView: UIView {
     lazy var inputTextView: UITextView = {
-        let textView = UITextView()
-        textView.textContainerInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 36)
+        let textView = GrowingTextView()
+        textView.textContainerInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 36)
         textView.backgroundColor = UIColor(hex: CIC.shared.M1)
-        textView.isScrollEnabled = false
         textView.layer.borderColor = UIColor(hex: CIC.shared.F2).cgColor
         textView.layer.borderWidth = 1
         textView.textColor = .white
+        textView.minHeight = 52
         textView.font = UIFont.systemFont(ofSize: 18, weight: .medium)
         textView.delegate = self
         return textView
     }()
-    
-    private var textViewHeightConstraint: NSLayoutConstraint!
-    private var defaultHeight: CGFloat = 52
     
     lazy var sendButton: UIButton = {
         let button = UIButton(type: .system)
@@ -51,9 +49,6 @@ class TextAreaView: UIView {
         [inputTextView, sendButton].forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
         let constant: CGFloat = 8
         
-        textViewHeightConstraint = inputTextView.heightAnchor.constraint(equalToConstant: defaultHeight)
-        textViewHeightConstraint.isActive = true
-        
         NSLayoutConstraint.activate([
             inputTextView.topAnchor.constraint(equalTo: topAnchor),
             inputTextView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: constant),
@@ -67,64 +62,11 @@ class TextAreaView: UIView {
         ])
     }
     
-    private func updateTextViewHeight() {
-        let numberOfLines = inputTextView.numberOfLines()
-        if inputTextView.contentSize.height > defaultHeight {
-            textViewHeightConstraint.constant = defaultHeight + defaultHeight
-            defaultHeight += defaultHeight
-        } else {
-            textViewHeightConstraint.constant = inputTextView.contentSize.height
-        }
-    }
-    
     @objc private func buttonDidPress() {
         print("send message")
     }
 }
 
 extension TextAreaView: UITextViewDelegate {
-    func textViewDidChange(_ textView: UITextView) {
-           updateTextViewHeight()
-    }
+    
 }
-
-extension UITextView {
-    func numberOfLines() -> Int {
-        guard let text = self.text else {
-            return 0
-        }
-        
-        let textHeight = self.contentSize.height
-        let lineHeight = self.font?.lineHeight ?? 0
-        let numberOfLines = Int(round(textHeight / lineHeight))
-        
-        return numberOfLines
-    }
-}
-
-//extension UITextView {
-//    func numberOfLines() -> Int {
-//        guard let text = self.text else {
-//            return 0
-//        }
-//
-//        let layoutManager = self.layoutManager
-//        let textContainer = self.textContainer
-//
-//        layoutManager.ensureLayout(for: textContainer)
-//
-//        let numberOfGlyphs = layoutManager.numberOfGlyphs
-//        var numberOfLines = 0
-//        var lineRange: NSRange = NSRange(location: NSNotFound, length: 0)
-//
-//        for index in 0..<numberOfGlyphs {
-//            layoutManager.lineFragmentRect(forGlyphAt: index, effectiveRange: &lineRange)
-//            if lineRange.location == NSNotFound {
-//                break
-//            }
-//            numberOfLines += 1
-//        }
-//
-//        return numberOfLines
-//    }
-//}
